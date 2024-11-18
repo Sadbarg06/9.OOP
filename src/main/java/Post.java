31ef71829b5eb3f371c0aee9f3e0961d8454947f
Sadbarg <sadbargo4ka93@yandex.ru>
2024-11-18 03:32:22 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,16 @@
+public class Post {
+
+    public String name; // поле для имени
+    public String passport; // паспорт
+    public String pastronymic; // отчество
+    public String phone; // телефон
+    public String surname; // фамилия
+    public boolean subscription; // новости о скидках в виде о смс
+    public FormDate birthday;
+}
+
+
+
+
+
+
